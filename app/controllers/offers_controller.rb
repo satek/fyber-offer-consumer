6df61b574aedd766ce78b845149cd08bf253cbc8
Marko Satek <satekm@gmail.com>
2015-04-26 19:35:59 +0200

    offers controller modification with views

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -6,9 +6,18 @@ class OffersController < ApplicationController
 
   def fetch
     @outcome = FyberApi.run(params[:fyber_api])
+    @partial = partial_to_send
     respond_to do |format|
       format.js
     end
+  end
+
+  private
+
+  def partial_to_send
+    return "offer_errors" unless @outcome.valid?
+    return "no_content" unless @outcome.result
+    "offer_list"
   end
 
 end
