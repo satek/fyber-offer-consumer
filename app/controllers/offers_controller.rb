54d198240c3d55854602fb7dc25ff0d2c53ceb23
Marko Satek <satekm@gmail.com>
2015-04-26 18:43:23 +0200

    offers controller with test

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,14 @@
 class OffersController < ApplicationController
 
   def index
-    
+    @fyber_api = FyberApi.new   
   end
+
+  def fetch
+    @outcome = FyberApi.run(params[:fyber_api])
+    respond_to do |format|
+      format.js
+    end
+  end
+
 end
--- test/controllers/offers_controller_test.rb
+++ test/controllers/offers_controller_test.rb
@@ -11,9 +11,26 @@ describe OffersController do
                    { controller: "offers", action: "fetch" })
   end
 
-  it "presents visitors with a welcome page" do
+  it "#index presents offers form" do
     get :index
     assert_response :success
+  end
+
+  it "#fetch returns rendered offers" do
+    VCR.use_cassette("fyber_api_call", match_requests_on: [:method, :host]) do
+      xhr :post, :fetch, fyber_api: FactoryGirl.attributes_for(:link)
+    end
+    outcome = assigns(:outcome)
+    result = outcome.result
+    keys = result.keys
+    keys.must_include "code"
+    keys.must_include "message"
+    keys.must_include "pages"
+    keys.must_include "count"
+    keys.must_include "information"
+    keys.must_include "offers"
+    result["information"].must_be_instance_of Hash
+    result["offers"].must_be_instance_of Array
   end
   
 end
